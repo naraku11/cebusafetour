245b0c84c0a4d631940ce3112e11f9a0ebbab6bd
naraku11 <kiuschool11@gmail.com>
2026-03-17 17:30:29 +0800

    v1.0

--- backend/src/config/firebase.js
+++ backend/src/config/firebase.js
@@ -8,7 +8,9 @@ const initFirebase = () => {
       credential: admin.credential.cert({
         projectId: process.env.FIREBASE_PROJECT_ID,
         clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-        privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+        privateKey: process.env.FIREBASE_PRIVATE_KEY
+          ?.replace(/^["']|["']$/g, '')   // strip accidental surrounding quotes
+          ?.replace(/\\n/g, '\n'),         // convert literal \n to real newlines
       }),
     });
   }
