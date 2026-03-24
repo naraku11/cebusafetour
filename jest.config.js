050a070b5cc98c84b544d3ddb55995315b18b6d2
naraku11 <kiuschool11@gmail.com>
2026-03-24 22:52:10 +0800

    v1.0

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  testEnvironment: 'node',
-  testMatch: ['**/tests/**/*.test.js'],
-  setupFiles: ['./tests/setup.js'],
-  // Prevent app.js from auto-starting the server
-  modulePathIgnorePatterns: ['<rootDir>/node_modules/'],
-  // Increase timeout for integration tests
-  testTimeout: 10000,
-};
