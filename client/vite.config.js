236e6ba8b53b8173612920806c3ff6a8f1ee1129
naraku11 <kiuschool11@gmail.com>
2026-04-24 23:06:14 +0800

    v1.0

--- client/vite.config.js
+++ client/vite.config.js
@@ -10,4 +10,13 @@ export default defineConfig({
       '/uploads': { target: 'http://localhost:5000', changeOrigin: true },
     },
   },
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: 'assets/[name].js',
+        chunkFileNames: 'assets/[name].js',
+        assetFileNames: 'assets/[name].[ext]',
+      },
+    },
+  },
 });
